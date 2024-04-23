c91aee28ea1b702a43a400125beebc19efa7b522
Elizabeth <106032842+Elizabeth1121@users.noreply.github.com>
2024-04-23 12:02:33 -0400

    Add files via upload
    FizzBuzz Daily Challenge Added

---
+++ FizzBuzz_Daily_Challenge/Program.cs
@@ -0,0 +1,27 @@
+﻿// loop for 1 to 100
+for (int i = 2; i < 100; i++)
+{
+    // if i is divisible by 3 and 5
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("{0} FizzBuzz", i);
+    }
+    // if i is divisible by 3
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("{0} Fizz", i);
+    }
+    // if i is divisible by 5
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("{0} Buzz", i);
+    }
+    // if i is not divisible by 3 or 5 or both
+    else
+    {
+        Console.WriteLine("{0}", i);
+    }
+}
+
+// keep console open 
+Console.ReadLine();
