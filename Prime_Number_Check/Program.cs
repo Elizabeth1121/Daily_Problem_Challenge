a60af7a4d4266d2a066b517a8562b6b7f14542c0
Elizabeth <106032842+Elizabeth1121@users.noreply.github.com>
2024-04-24 15:04:43 -0400

    Add files via upload
    Prime Number Check Daily Problem

---
+++ Prime_Number_Check/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution sol = new Solution();
+        // this works
+        //Console.WriteLine(sol.IsPalindrome(12));
+
+        // test 2
+        Console.WriteLine(sol.IsPalindrome2(12));
+    }
+}
+
+public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        for(int i = 2; i < x; i++)
+        {
+            if(x != i && x % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public bool IsPalindrome2(int x)
+    {
+        int counter = x;
+
+        if(x !=0 && x % counter == 0)
+        {
+            return false;
+        }
+        if(counter == 1 && counter != x)
+        {
+            return true;
+        }
+        return IsPalindrome2(counter - 1);
+    }
+}
