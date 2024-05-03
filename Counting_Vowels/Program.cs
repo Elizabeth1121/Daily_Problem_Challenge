bb2c0ad6f61fab53902ff4bb89a66e38e376a7c7
Elizabeth <106032842+Elizabeth1121@users.noreply.github.com>
2024-05-03 14:53:07 -0400

    Add files via upload
    Added Counting Vowels Challenge

---
+++ Counting_Vowels/Program.cs
@@ -0,0 +1,21 @@
+﻿// getting user input
+string input = Console.ReadLine();
+
+// hashset for vowels upper and lower case
+HashSet<char> vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+// creating a counter for vowels
+int counter = 0;
+
+// loop through the input
+foreach (char c in input)
+{
+    if(vowels.Contains(c))
+    {
+        counter++;
+    }
+}
+
+// output the number of vowels
+Console.WriteLine(counter);
+Console.ReadLine();
